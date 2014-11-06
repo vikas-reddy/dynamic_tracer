0ab886d26cd62a308cd62f8bfb72b65eae8217f9
Vikas Reddy <vikasreddy.com@gmail.com>
2014-11-06 18:32:10 +0530

    0632

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,6 +10,7 @@ $(function() {
 
       var url = "http://" + environment + ":" + port + "/" + service + "service/" + op + "?" + expression;
       $('#generated-url').html(url);
+      $('#fetch-trace').removeClass('disabled');
     });
 
     $('#fetch-trace').click(function(e) {
@@ -17,10 +18,15 @@ $(function() {
         url: '/fetch-trace',
         method: 'GET',
         data: {'url': $('#generated-url').html()},
+        beforeSend: function() {
+          $('#fetch-trace').addClass('disabled');
+        },
         success: function(resp) {
+          $('#fetch-trace').removeClass('disabled');
           $('#response').html(resp);
         },
         error: function(resp) {
+          $('#fetch-trace').removeClass('disabled');
           $('#response').html(resp);
         }
       });
