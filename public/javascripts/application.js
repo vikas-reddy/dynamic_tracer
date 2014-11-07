a7c971a7a3530055e02c74b721b180bf27a7b9e5
Vikas Reddy <vreddy@castlighthealth.com>
2014-11-07 08:58:29 +0000

    0858

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,12 @@
 $(function() {
+  function encodeExpression(expr) {
+    // expr = expr.replace(/({|})/g, encodeURIComponent('$1'));
+    expr = expr.replace(/( |\t|\n)/g, "+");
+    expr = expr.replace(/{/g, "%7B");
+    expr = expr.replace(/}/g, "%7D");
+    return expr;
+  }
+
   $(document).ready(function() {
     $('form').submit(function(e) {
       e.preventDefault();
@@ -6,9 +14,9 @@ $(function() {
       var service = $('#service').val();
       var port = $('#service').find('option:selected').data('port');
       var op = $('#op').val();
-      var expression = $('#expression').val();
+      var expression = encodeExpression($('#expression').val());
 
-      var url = "http://" + environment + ":" + port + "/" + service + "service/" + op + "?" + expression;
+      var url = "http://" + environment + ":" + port + "/" + service + "service/" + op + "?expression=" + expression;
       $('#generated-url').html(url);
       $('#fetch-trace').removeClass('disabled');
     });
